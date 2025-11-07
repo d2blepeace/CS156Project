265ca39c79e695137a9d7e633c09fb50d9699814
Thai Nguyen <nguyenhoathai.nht@gmail.com>
2025-11-07 15:13:59 -0800

    Complete the functions for reading and parsing csv file from folder 1 and 2

--- src/constants.py
+++ src/constants.py
@@ -1,14 +1,12 @@
-# constants.py
-
 # folder naming convention from dataset: "0.Stand", "1.Sit"
 CLASS_FOLDER_PATTERN = r"^(\d+)\.(.+)$"
 
-# 2. default dataset split
+# Use the pre-processed split first
 DEFAULT_SPLIT_DIR = "2.Trimmed_interpolated_data"
 
-# Human activity label 
-CLASS_NAME = [
-    "Stand", "Sit", "Talk-sit", "Talk-Stand", "Stand-sit", "Lay", "Lay-stand",
-    "Pick", "Jump", "Push-up", "Sit-up", "Walk", "Walk-backwards",
-    "Walk-circle", "Run", "Stair-up", "Stair-down", "Table-tennis"
+# Human activity labels (index-aligned: 0..17)
+CLASS_NAMES = [
+    "Stand","Sit","Talk-sit","Talk-stand","Stand-sit","Lay","Lay-stand",
+    "Pick","Jump","Push-up","Sit-up","Walk","Walk-backwards",
+    "Walk-circle","Run","Stair-up","Stair-down","Table-tennis"
 ]
